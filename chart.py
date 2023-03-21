090ae008da71c72a35bda7520c1bad7b3ee86cc7
ywakida <122138872+ywakida@users.noreply.github.com>
2023-03-22 07:08:19 +0900

    Update chart.py

--- chart.py
+++ chart.py
@@ -9,7 +9,7 @@ basepath = './'
 encode = 'utf-8'
 
 # [1m, 2m, 5m, 15m, 30m, 60m, 90m, 1h, 1d, 5d, 1wk, 1mo, 3mo]
-def update_chart_csv(folder_path, ticker, interval):
+def update_chart_csv(folder_path, ticker, interval, is_save=True):
     """ チャートのCSVの作成
 
     Args:
@@ -20,11 +20,12 @@ def update_chart_csv(folder_path, ticker, interval):
     if not os.path.exists(file_name):
         
         currency = yfinance.Ticker(f'{ticker}=X')
-        new_chart = currency.history(period="max", interval=interval, progress=False)
+        new_chart = currency.history(periods="max", interval=interval, progress=False)
         if not new_chart.empty: # 空データでない
-            if len(new_chart) > 1: # ヘッダのみでない                
-                new_chart.to_csv(file_name, header=True) # 保存
-                print(f'{file_name} is created.')       
+            if len(new_chart) > 1: # ヘッダのみでない
+                if is_save == True:    
+                    new_chart.to_csv(file_name, header=True) # 保存
+                    print(f'{file_name} is created.')       
     
     else: 
         # csvファイルを読み取り、最新日付を取得する
@@ -46,8 +47,10 @@ def update_chart_csv(folder_path, ticker, interval):
         chart_diff = pandas.DataFrame()
         currency = yfinance.Ticker(f'{ticker}=X')
         
-        chart_diff = currency.history(period=f'max', interval=interval, progress=False)            
+        chart_diff = currency.history(periods=f'max', interval=interval, progress=False)            
         chart_diff.index = pandas.to_datetime(chart_diff.index)
+        
+        chart_diff = chart_diff[:-1] # 末尾1行を削除
 
         if not chart_diff.empty:
             if len(chart_diff) > 1:  
@@ -56,106 +59,26 @@ def update_chart_csv(folder_path, ticker, interval):
                 chart = chart[~chart.index.duplicated(keep='last')] # 重複があれば最新で更新する
                 chart.sort_index(axis='index', ascending=True, inplace=True)
                 chart.dropna(how='all', inplace=True)
-                chart.to_csv(file_name, header=True) # 保存
-                print(f"{file_name} is updated.")
+                
+                print(chart.tail(10))
+                
+                # latest_date = chart.index[-1]                
+                # print(latest_date)
+                # print(latest_date.second)
+                # print(latest_date.minute)
+                
+                
+                if is_save == True:  
+                    chart.to_csv(file_name, header=True) # 保存
+                    print(f"{file_name} is updated.")
                
         else:
             print(f'{file_name} is incorrect.')
         
-def validate_chart_csv(folder_path, interval, ticker):
-    """差分更新更新
-
-    Args:
-        folder_path (_type_): _description_
-        ticker (_type_): _description_
-    """
-    file_name = f'{folder_path}/{ticker}_{interval}.csv'
-    if os.path.exists(file_name):
-        
-        # csvファイルを読み取り、最新日付を取得する
-        chart =  pandas.read_csv(file_name, index_col=0, parse_dates=True)
-        
-        chart['timesatampdiff'] = chart.index.to_series().diff().dt.total_seconds() % 180000
-        # chart['timesatampdiff'] = chart['timesatampdiff'] % 180000
-        # chart['timesatampdiff'] = chart['timesatampdiff'].diff()
-        
-        
-        print(chart['timesatampdiff'])
-        
-
-    else:
-        print(f"{file_name} is not existed.")
-
-
-
-def format_chart_csv(folder_path, interval, ticker):
-    file_name = f'{folder_path}/{ticker}_{interval}.csv'
-    if os.path.exists(file_name):
-        
-        # csvファイルを読み取り、最新日付を取得する
-        chart =  pandas.read_csv(file_name, index_col=0, parse_dates=True)
-        
-        # print(chart.tail(10)) 
-        if (interval == '1h'):
-            chart = chart.asfreq('1H')
-        
-        if (interval == '15m'):
-            chart = chart.asfreq('15T')
-    
-        if (interval == '5m'):
-            chart = chart.asfreq('5T')       
-            
-        # print(chart.tail(10)) 
-        chart.to_csv(file_name, header=True) # 保存
-        
-def merge_chart_csv(folder_path_base, folder_path_add, folder_path_out, interval, currency):
-    """差分更新更新
-
-    Args:
-        folder_path (_type_): _description_
-        ticker (_type_): _description_
-    """
-    file_name1 = f'{folder_path_base}/{currency}_{interval}.csv'
-    file_name2 = f'{folder_path_add}/{currency}_{interval}.csv'
-    file_name3 = f'{folder_path_out}/{currency}_{interval}.csv'
-    if os.path.exists(file_name1) and os.path.exists(file_name2):
-        
-        # csvファイルを読み取り、最新日付を取得する
-        chart_base =  pandas.read_csv(file_name1, index_col=0, parse_dates=True)
-        chart_add =  pandas.read_csv(file_name2, index_col=0, parse_dates=True)
-
-        chart_base = pandas.concat([chart_base, chart_add])
-        chart_base = chart_base[~chart_base.index.duplicated(keep='last')] # 重複があれば最新で更新する
-        chart_base.sort_index(axis='index', ascending=True, inplace=True)
-        if (interval == '1h'):
-            chart_base = chart_base.asfreq('1H')
-        
-        if (interval == '15m'):
-            chart_base = chart_base.asfreq('15T')
-    
-        if (interval == '5m'):
-            chart_base = chart_base.asfreq('5T')
-            
-        chart_base.dropna(how='all', inplace=True)
-        chart_base.reset_index(drop=True) # インデックスの降り直し　だけど、datetimeindexだから意味ないかも 
-        chart_base.to_csv(file_name3, header=True) # 保存
-        
-        # chart_base.merge(chart_add)
-        # chart_base = pandas.concat([chart_base, chart_add])
-        # chart_base.reset_index(drop=True) # インデックスの降り直し　だけど、datetimeindexだから意味ないかも 
-        # chart_base.drop_duplicates(keep='last', inplace=True) # 重複があれば最新で更新する
-        # chart_base.to_csv(file_name3, header=True) # 保存
-        print(f"{file_name3} is updated.")
-        
-    else:
-        print(f"file is not existed.")
-    
 def task():
     folder = 'chart_csv'
     intervals = ['5m', '15m', '1h', '1d', '1wk']
     currencies = ['USDJPY', 'EURJPY', 'GBPJPY', 'AUDJPY', 'NZDJPY', 'CADJPY', 'EURUSD', 'GBPUSD', 'AUDUSD', 'NZDUSD', 'CADUSD']
-    # intervals = ['5m']
-    # currencies = ['USDJPY']
     
     for currency in currencies:
         print(currency, ":")
@@ -163,8 +86,6 @@ def task():
         for interval in intervals:
             print(" - ", interval)
             update_chart_csv(folder, currency, interval) 
-            # format_chart_csv('_chart_csv', interval, currency)  
-            # validate_chart_csv(folder, interval, currency)
 
 
 if __name__ == "__main__":
